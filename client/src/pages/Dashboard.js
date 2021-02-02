c00237e924bf285a94aa173ce9fd882a01095012
Greg Bastianelli <greg@gerrg.com>
2021-02-02 15:08:24 -0500

    add header, logo and logout button

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -9,25 +9,41 @@ const Dashboard = ({ setToken }) => {
   };
   return (
     <Header>
-      <Button onClick={() => logout()}>
-        <FaSignOutAlt size={32} />
-      </Button>
+      <Inner>
+        <Logo href="#">To Do</Logo>
+        <LogoutButton onClick={() => logout()}>
+          <FaSignOutAlt size={32} />
+        </LogoutButton>
+      </Inner>
     </Header>
   );
 };
 
-const Button = styled.button`
+const LogoutButton = styled.button`
   border: 0;
   color: inherit;
   background: transparent;
   cursor: pointer;
+  color: var(--color-secondary);
 `;
 
 const Header = styled.header`
   width: 100%;
   background-color: var(--color-dark-grey);
-  padding: 1rem;
-  color: var(--color-secondary);
+  padding: 0.5rem 0;
+`;
+
+const Inner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 0.5rem;
+`;
+
+const Logo = styled.a`
+  color: #fff;
+  font-weight: 600;
+  text-decoration: none;
 `;
 
 export default Dashboard;

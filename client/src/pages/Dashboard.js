73d5b2a3af464d3da10a82653c2f0fa935bf6db4
Greg Bastianelli <greg@gerrg.com>
2021-02-02 14:59:19 -0500

    login/logout

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+import { FaSignOutAlt } from "react-icons/fa";
 
-const Dashboard = () => {
-  return <p>dashboard</p>;
+const Dashboard = ({ setToken }) => {
+  const logout = () => {
+    setToken(null);
+    localStorage.removeItem("js-tasker");
+  };
+  return (
+    <Header>
+      <Button onClick={() => logout()}>
+        <FaSignOutAlt size={32} />
+      </Button>
+    </Header>
+  );
 };
+
+const Button = styled.button`
+  border: 0;
+  color: inherit;
+  background: transparent;
+  cursor: pointer;
+`;
+
+const Header = styled.header`
+  width: 100%;
+  background-color: var(--color-dark-grey);
+  padding: 1rem;
+  color: var(--color-secondary);
+`;
 
 export default Dashboard;
